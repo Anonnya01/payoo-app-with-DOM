37d35c2878cc402dc564c4484a39818898578de1
Anonnya01 <anonnya1999@gmail.com>
2025-08-23 12:35:45 +0600

    cashout function done

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
-//------------Get input values----------//
-const validPin =1234;
+//------------Get input values for add money ----------//
+const validPin = 1234;
 
 document
   .getElementById("btn-add-money")
@@ -9,19 +9,19 @@ document
     const banks = document.getElementById("select-bank").value;
     const accountNum = document.getElementById("bank-number").value;
     const addedAmount = parseInt(document.getElementById("amount-added").value);
-    const bankPin = parseInt(document.getElementById("bank-pin").value)
+    const bankPin = parseInt(document.getElementById("bank-pin").value);
 
     const availableCash = parseInt(
       document.getElementById("available-cash").innerText
     );
 
     // -------conditions for checking-------//
-    if (accountNum.length < 11 ) {
+    if (accountNum.length < 11) {
       alert("Enter Correct Number");
       return;
     }
-    if( bankPin !== validPin){
-    alert("Enter Correct Pin");
+    if (bankPin !== validPin) {
+      alert("Enter Correct Pin");
       return;
     }
 
@@ -31,15 +31,32 @@ document
     const newAmountBalance = addedAmount + availableCash;
     document.getElementById("available-cash").innerText = newAmountBalance;
   });
+// --------------------------End---------------------//
 
+// -------------cash out features----------//
+
+document.getElementById("btn-cashout").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = parseInt(
+    document.getElementById("amount-withdraw").value
+  );
+  const availableBalance = parseInt(
+    document.getElementById("available-cash").innerText
+  );
+  console.log(amount, availableBalance);
+  const newAvailableCash = availableBalance - amount
+  document.getElementById("available-cash").innerText = newAvailableCash;
+  
+});
 
 //  --------toggling Features------------//
 
-document.getElementById("add-card").addEventListener('click',function(){
-   document.getElementById("cashout-box").style.display ="none"
-   document.getElementById("add-money-box").style.display ="block"
-})
-document.getElementById("cashout-card").addEventListener('click',function(){
-   document.getElementById("add-money-box").style.display ="none"
-   document.getElementById("cashout-box").style.display ="block"
-})
+document.getElementById("add-card").addEventListener("click", function () {
+  document.getElementById("cashout-box").style.display = "none";
+  document.getElementById("add-money-box").style.display = "block";
+});
+document.getElementById("cashout-card").addEventListener("click", function () {
+  document.getElementById("add-money-box").style.display = "none";
+  document.getElementById("cashout-box").style.display = "block";
+});
