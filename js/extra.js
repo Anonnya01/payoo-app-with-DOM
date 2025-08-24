a023db809f2115b5857ebcae21736a7c7e2a70e6
Anonnya01 <anonnya1999@gmail.com>
2025-08-24 12:33:47 +0600

    transaction section done

--- js/extra.js
+++ js/extra.js
@@ -87,3 +87,78 @@
 //   }
 //   document.getElementById("pay-bill-box").style.display = "block";
 // });
+
+
+
+// ---------------toggle with reused function-- for card----------//
+
+// document.getElementById("add-card").addEventListener("click", function () {
+//   toggleBox("add-money-box");
+//   const fromButton = document.getElementsByClassName("form-btn")
+ 
+//   for(const btn of fromButton){
+//     btn.classList.remove("border-[#0874F2]","bg-[#0874F20D]")
+//     btn.classList.add("border-gray-300")
+//   }
+//     document.getElementById("add-card").classList.remove("border-gray-300")
+//   document.getElementById("add-card").classList.add("border-[#0874F2]","bg-[#0874F20D]")
+// });
+
+// // --------cash out-----------//
+
+// document.getElementById("cashout-card").addEventListener("click", function () {
+//   toggleBox("cashout-box");
+
+//    const fromButton = document.getElementsByClassName("form-btn")
+ 
+//   for(const btn of fromButton){
+//     btn.classList.remove("border-[#0874F2]","bg-[#0874F20D]")
+//     btn.classList.add("border-gray-300")
+//   }
+//     document.getElementById("cashout-card").classList.remove("border-gray-300")
+//   document.getElementById("cashout-card").classList.add("border-[#0874F2]","bg-[#0874F20D]")
+// });
+
+// // -------------transfer------//
+
+// document.getElementById("transfer-card").addEventListener("click", function () {
+//   toggleBox("transfer-box");
+
+//    const fromButton = document.getElementsByClassName("form-btn")
+ 
+//   for(const btn of fromButton){
+//     btn.classList.remove("border-[#0874F2]","bg-[#0874F20D]")
+//     btn.classList.add("border-gray-300")
+//   }
+//     document.getElementById("transfer-card").classList.remove("border-gray-300")
+//   document.getElementById("transfer-card").classList.add("border-[#0874F2]","bg-[#0874F20D]")
+// });
+
+// // -------bonus-----------//
+
+// document.getElementById("bonus-card").addEventListener("click", function () {
+//   toggleBox("bonus-box");
+
+//    const fromButton = document.getElementsByClassName("form-btn")
+ 
+//   for(const btn of fromButton){
+//     btn.classList.remove("border-[#0874F2]","bg-[#0874F20D]")
+//     btn.classList.add("border-gray-300")
+//   }
+//     document.getElementById("bonus-card").classList.remove("border-gray-300")
+//   document.getElementById("bonus-card").classList.add("border-[#0874F2]","bg-[#0874F20D]")
+// });
+
+// // -------------bill-----------//
+// document.getElementById("bill-card").addEventListener("click", function () {
+//   toggleBox("pay-bill-box");
+
+//   const fromButton = document.getElementsByClassName("form-btn")
+ 
+//   for(const btn of fromButton){
+//     btn.classList.remove("border-[#0874F2]","bg-[#0874F20D]")
+//     btn.classList.add("border-gray-300")
+//   }
+//     document.getElementById("bill-card").classList.remove("border-gray-300")
+//   document.getElementById("bill-card").classList.add("border-[#0874F2]","bg-[#0874F20D]")
+// });
--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 //------------Get input values for add money ----------//
 const validPin = 1234;
 const validOutPin = 2222;
+const transaction = []
 
 // ------reuse function--Convert to num--//
 
@@ -44,6 +45,20 @@ function toggleBox(id) {
   document.getElementById(id).style.display = "block";
 }
 
+// -----function---toggle for CARD-----//
+function toggleCard(id) {
+  const fromButton = document.getElementsByClassName("form-btn");
+
+  for (const btn of fromButton) {
+    btn.classList.remove("border-[#0874F2]", "bg-[#0874F20D]");
+    btn.classList.add("border-gray-300");
+  }
+  document.getElementById(id).classList.remove("border-gray-300");
+  document
+    .getElementById(id)
+    .classList.add("border-[#0874F2]", "bg-[#0874F20D]");
+}
+
 // ---------ADD MONEY------//
 document
   .getElementById("btn-add-money")
@@ -74,17 +89,26 @@ document
     const newAmountBalance = addedAmount + availableCash;
 
     getUpdatedText(newAmountBalance);
+
+    // ----------transaction-----------//
+      const data ={
+        name: "Add Money",
+        date:new Date().toLocaleTimeString()
+      }
+      transaction.push(data)
+      console.log(transaction);
   });
 // --------------------------End---------------------//
 
-// -------------cash out features----------//
+
+// -------------CASH OUT features----------//
 
 document.getElementById("btn-cashout").addEventListener("click", function (e) {
   e.preventDefault();
 
   const outNumber = document.getElementById("out-number").value;
   const amount = getInputNum("amount-withdraw");
-  const bankOutPin = parseInt(document.getElementById("out-pin").value);
+  const bankOutPin = getInputNum("out-pin");
   const availableBalance = getInnerText("available-cash");
 
   // -------conditions for checking-------//
@@ -98,31 +122,115 @@ document.getElementById("btn-cashout").addEventListener("click", function (e) {
   }
 
   console.log(amount, availableBalance);
+
+  // --------subtraction---------//
+
   const newAvailableCash = availableBalance - amount;
 
   getUpdatedText(newAvailableCash);
+
+   // ----------transaction-----------//
+      const data ={
+        name: "Cash Out Money",
+        date:new Date().toLocaleTimeString()
+      }
+      transaction.push(data)
+
+      console.log(transaction);
+      
 });
+
+
+// ------------transaction features----------//
+document.getElementById("transaction-card")
+.addEventListener('click',function () {
+  
+  const dataView= document.getElementById("transaction-output")
+
+  dataView.innerHTML =""
+  for(const data of transaction){
+    const div = document.createElement("div")
+    div.innerHTML =`
+     <div class="flex items-center justify-between p-4 mb-4 bg-white border-gray-300 border-1 rounded-xl">
+            
+            <div class="flex items-center gap-3">
+              
+             <div class="bg-[#0808080D] rounded-full p-3">
+               <img src="assets/wallet1.png" alt="">
+              </div>
+             <div><h1 class="font-semibold">${data.name}</h1>
+              <p class="text-[14px] text-[#08080880]">${data.date}</p>
+            </div>
+           
+            </div>
+            <div>
+              <img src="assets/Frame.png" alt="">
+            </div>
+          </div>
+    `
+
+    dataView.appendChild(div)
+  }
+  
+})
+
 
 // ------------------End------------//
 
-// ---------------toggle with reused function------------//
+
+// ---------------toggle with reused function-----------//
+
+
+// -------Add-card-----------//
 
 document.getElementById("add-card").addEventListener("click", function () {
+
   toggleBox("add-money-box");
+
+  toggleCard("add-card");
 });
+
+// --------cash out-----------//
 
 document.getElementById("cashout-card").addEventListener("click", function () {
+
   toggleBox("cashout-box");
+
+  toggleCard("cashout-card");
 });
+
+// -------------transfer------//
 
 document.getElementById("transfer-card").addEventListener("click", function () {
+
   toggleBox("transfer-box");
+
+  toggleCard("transfer-card");
 });
+
+// -------bonus-----------//
 
 document.getElementById("bonus-card").addEventListener("click", function () {
+
   toggleBox("bonus-box");
+
+  toggleCard("bonus-card");
 });
 
+// -------------bill-----------//
+
 document.getElementById("bill-card").addEventListener("click", function () {
+
   toggleBox("pay-bill-box");
+
+  toggleCard("bill-card");
+});
+
+// -----------transaction-------------//
+
+document.getElementById("transaction-card").addEventListener("click", function () {
+
+  toggleBox("transaction-box");
+
+  toggleCard("transaction-card");
 });
